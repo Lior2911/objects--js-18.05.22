8658dd9650f8bbd6e1e6f1b7c68068db7d76ad89
lior2911 <aytegev1@gmail.com>
2022-05-18 20:30:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+// 14.	צרו פונקציה המקבלת כארגומנט מערך של גילים, מייצרת ומחזירה מערך חדש המכיל רק גילים מעל 20.
+
+// function getAges(someArray){
+// var someArray = [12,23,34,14,43,16]
+// newArray = []
+// for(var i = 0 ; i<someArray.length;i++){
+//   if(someArray[i]<20){
+//     newArray += someArray[i]
+//   }
+
+// }
+// return newArray
+// }
+
+// צרו פונקציה המקבלת מהמשתמש שם פרטי, אם השם מתחיל ב A היא מדפיסה לקונסול "hello" אחרת מדפיסה "bye".
+
+// function getA(){
+//   var firstName = prompt("enter your name")
+//     if(firstName[0]=="A"){
+//       console.log("hello") 
+//     }
+//     else{
+//       console.log("bye")
+//     }
+// }
+// // getA()
+
+// צרו פונקציה המקבלת מהמשתמש שם משפחה, אם השם מסתיים ב E היא מחזירה "yes" אחרת היא מחזירה "no".
+// // יש ללמוד את שקופית 27 ולתרגל ברשת.
+
+// function getE(){
+//   var lastName = prompt("enter your last name")
+//   if(lastName[lastName.length-1] =="E"){
+    
+//     console.log("yes")
+//   }
+//   else{
+//     console.log("no")
+//   }
+// }
+// getE()
+
+// כב פונקציה ב-JS המקבלת מחרוזת ומחזירה uppercase שלהתו
+
+// function getUp(){
+//   var word = "hello world"
+//   var upWord = word.toUpperCase()
+//   console.log(upWord) 
+// }
+// // getUp()
+
+// כתוב פונקציה ב-JS המקבלת מחרוזת עם מספרים מופרדים בעזרת פסיק ומחזירה מערך של המספרים.
+// לדוגמא עבור "12,3,455" יוחזר מערך עם המספרים 12,3,455
+// 
+
+// function getNum (){
+//   var num1 = [1,2,34,5,67]
+//   var num2 = []
+//   for(var i = 0 ; i<num1.length;i++){
+//     num2 += num1[i]
+//   }
+//   console.log(num2)
+// }
+// getNum()
+
+// כתוב פונקציה ב-JS המקבלת מחרוזת של key:value מופרדים בעזרת פסיק. יש להחזיר את אורך מחרוזת value הארוכה ביותר.
+// לדוגמא עבור המחרוזת 7 (האורך של "krasney")
+
+function getLongest(str){
+ 
+  var long = ""
+  for(var i = 0;i<str.length[i];i++){
+    
+   
+
+  }
+}
+getLongest("name:lior,family:aytegev,city:ashkelon")
